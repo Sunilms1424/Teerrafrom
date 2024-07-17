89a6cbb456c9e373127a9a96169c7a76d307c977
Sunilms1424 <132062594+Sunilms1424@users.noreply.github.com>
2024-07-17 08:18:48 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,1 +1,8 @@
+provider "aws" {
+    region = "us-east-1" #set your desired AWS region
+}
 
+resource "aws_instance" "example" {
+    ami   = "ami-04a81a99f5ec58529" #specify an appropriate AMI ID (ex: here i took ubuntu ami )
+    instance_type = 't2.micro"
+}
